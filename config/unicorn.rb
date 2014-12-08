13b02998657d12a855bcdde1249097f1fbae168b
Nico Tonozzi <nico@nicot.us>
2014-12-07 23:46:02 -0700

    unicorn stuff

---
+++ config/unicorn.rb
@@ -0,0 +1,12 @@
+working_directory "/home/rails/SpeedyTyper"
+
+pid "/var/www/SpeedyTyper/pids/unicorn.pid"
+
+stderr_path "/home/rails/SpeedyTyper/log/unicorn.log"
+stderr_out "/home/rails/SpeedyTyper/log/unicorn.log"
+
+listen "/tmp/unicorn.[SpeedyTyper].sock"
+listen "/tmp/unicorn.SpeedyTyper.sock"
+
+worker_processes 2
+timeout 30
